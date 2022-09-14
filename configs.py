218af2d2279ceb3ed14f5e4099564ed378bfaa58
Edu <efg1997@gmail.com>
2022-09-14 19:20:40 +0200

    Subida del archivo de configuración

---
+++ configs.py
@@ -0,0 +1,17 @@
+# Youtube API 
+DEVELOPER_KEY = "" 
+# Introduce API Key for the use of YT Data API. 
+# To obtain API key check: https://developers.google.com/youtube/v3/getting-started
+
+# Redis DB
+# Introduce host and port of the Redis Database
+# You can follow the tutorial for the instalation with the Redis docker image:
+# https://developer.redis.com/create/docker/ 
+host = ""
+port = ()
+seconds_to_expire = 60*60*24 # number of seconds that takes a cached data set to expire. Set to 24h by default 
+
+
+# Model
+model_name = "ml-models/sia.sav" # Insert other models in ml-models modules to change the model use in the text clasification  
+
